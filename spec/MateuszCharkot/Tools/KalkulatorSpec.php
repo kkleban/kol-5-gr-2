156387bbbd1c28f16e422ca7d59146c9de314ab5
MateuszCharkot <mateusz.charkot@gmail.com>
2015-05-07 13:24:55 +0200

    OBJProstopadłościanu

--- spec/MateuszCharkot/Tools/KalkulatorSpec.php
+++ spec/MateuszCharkot/Tools/KalkulatorSpec.php
@@ -11,4 +11,17 @@ class KalkulatorSpec extends ObjectBehavior
     {
         $this->shouldHaveType('MateuszCharkot\Tools\Kalkulator');
     }
+
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setH(456)->getH()->shouldReturn(456);
+    }
+        function it_should_calculate_multiplication()
+    {
+    $this->setA(5)->setB(7)->setH(4)->multiplication()->shouldReturn(140);
+    }
 }
+
+

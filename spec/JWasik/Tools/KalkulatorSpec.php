498fab1ea288fa46437c77ba2d4baad67064d59c
Jarosław Wąsik <wasik.jaroslaw@outlook.com>
2015-05-07 13:30:42 +0200

    JWasik/kalkulator: Implementation

--- spec/JWasik/Tools/KalkulatorSpec.php
+++ spec/JWasik/Tools/KalkulatorSpec.php
@@ -11,4 +11,12 @@ class KalkulatorSpec extends ObjectBehavior
     {
         $this->shouldHaveType('JWasik\Tools\Kalkulator');
     }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setH(432)->getH()->shouldReturn(432);
+    }
+
 }
--- src/JWasik/Tools/Kalkulator.php
+++ src/JWasik/Tools/Kalkulator.php
@@ -4,4 +4,53 @@ namespace JWasik\Tools;
 
 class Kalkulator
 {
+    private $a;
+    private $b;
+    private $h;
+    
+    public function setA($a)
+    {
+        $this->a = $a;
+        
+        return $this;
+    }
+    
+    public function getA()
+    {
+        return $this->a;
+    }
+
+    public function setB($b)
+    {
+        $this->b = $b;
+        
+        return $this;
+    }
+    
+    public function getB()
+    {
+        return $this->b;
+    }
+
+    public function setH($h)
+    {
+        $this->h = $h;
+        
+        return $this;
+    }
+    
+    public function getH()
+    {
+        return $this->h;
+    }
+    
+    function it_should_calculate_field()
+    {
+        $this->setA(2)->setB(3)->setH(2)->field()->shouldReturn(12);
+    }
+    
+    public function field()
+    {
+        return $this->a * $this->b * $this->h;
+    }   
 }

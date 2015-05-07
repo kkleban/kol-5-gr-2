93ae88b5ec65b6bb7b3859278c5c8964efb77ca3
MichalDebowski <mic.debowski@gmail.com>
2015-05-07 13:48:18 +0200

    kalkulator phpspec

--- spec/MichalDebowski/Tools/KalkulatorSpec.php
+++ spec/MichalDebowski/Tools/KalkulatorSpec.php
@@ -11,4 +11,16 @@ class KalkulatorSpec extends ObjectBehavior
     {
         $this->shouldHaveType('MichalDebowski\Tools\Kalkulator');
     }
+
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setH(456)->getH()->shouldReturn(456);
+    }
+    
+    function prosto()
+    {
+    $this->setA(4)->setB(5)->setH(3)->obj()->shouldReturn(60);
+    }
 }

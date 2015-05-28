9497b950f562381019b99085d329aa282aa202e0
kkleban <kam.kleban@hotmail.com>
2015-05-28 12:59:25 +0200

    kkleban kalkulator

--- spec/kkleban/Tools/KalkulatorSpec.php
+++ spec/kkleban/Tools/KalkulatorSpec.php
@@ -11,4 +11,10 @@ class KalkulatorSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kkleban\Tools\Kalkulator');
     }
+      function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setC(567)->getC()->shouldReturn(567);
+    }
 }
--- src/kkleban/Tools/Kalkulator.php
+++ src/kkleban/Tools/Kalkulator.php
@@ -4,4 +4,49 @@ namespace kkleban\Tools;
 
 class Kalkulator
 {
+      private $a;
+    private $b;
+    private $c;
+
+    public function setA($a)
+    {
+        $this->a = $a;
+
+        return $this;
+    }
+
+    public function setB($b)
+    {
+        $this->b = $b;
+
+        return $this;
+    }
+     public function setC($c)
+    {
+        $this->c = $c;
+
+        return $this;
+    }
+
+    public function getA()
+    {
+        return $this->a;
+    }
+
+    public function getB()
+    {
+        return $this->b;
+    }
+     public function getC()
+    {
+        return $this->c;
+    }
+    public function obj()
+{
+    return $this->a * $this->b * $this->c;
+}
+    function it_should_calculate_obj()
+{
+    $this->setA(3)->setB(4)->setC(5)->obj()->shouldReturn(60);
+}
 }
